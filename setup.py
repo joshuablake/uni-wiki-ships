5d4039e1ea8b65c78959c92e3e6d23d7b42cffc0
Joshua Blake <joshbblake@gmail.com>
2013-01-02 10:34:15 +0000

    Add a setup.py

---
+++ setup.py
@@ -0,0 +1,22 @@
+from distribute_setup import use_setuptools
+use_setuptools()
+from setuptools import setup
+
+setup(
+      name='Uni Wiki Ships',
+      version='0.1.0',
+      packages=['uni_wiki_ships'],
+      entry_points={
+        'console_scripts': [
+            'wikiships = uni_wiki_ships.main:main',
+        ]
+      },
+      
+      author="Joshua Blake",
+      author_email="joshbblake@gmail.com",
+      description="This is an Example Package",
+      license="BSD",
+      keywords="hello world example examples",
+      url="https://github.com/joshuablake/uni-wiki-ships",
+      long_description=open('README.rst').read(),
+)
