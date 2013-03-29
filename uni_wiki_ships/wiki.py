dd44198adb22263d6f85f8e53c89e9bfe55afbc4
Joshua Blake <joshbblake@gmail.com>
2013-03-29 11:08:29 +0000

    Add barebones wiki.py file

---
+++ uni_wiki_ships/wiki.py
@@ -0,0 +1,10 @@
+class Wiki(object):
+    def __init__(self, url, delay):
+        self._url = url
+        self.delay = delay
+    
+    def login(self, username, password):
+        pass
+    
+    def get_pages(self, pages):
+        pass
